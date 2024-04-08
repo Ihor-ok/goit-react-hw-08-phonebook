c13446c5cb5af432b32fe869fc4f69312cd7f3a8
Ihor <kovaluk_igor@ukr.net>
2024-04-08 18:54:38 +0200

    Update AuthNav.js

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,16 +1,33 @@
 import { NavLink } from 'react-router-dom';
 import css from './AuthNav.module.css';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F5F843',
+      light: '#E9DB5D',
+      dark: '#A29415',
+      contrastText: '#0b0b0b',
+    },
+    contrastText: '#0b0b0b',
+    contrastThreshold: 4.5,
+  },
+});
 
 export const AuthNav = () => {
   return (
     <div>
-      <Button style={{ marginRight: 10 }} variant="contained"><NavLink className={css.link} to="/register">
-        Register
-      </NavLink></Button>
-      <Button  variant="contained"><NavLink className={css.link} to="/login">
-        Log In
-      </NavLink></Button>
+      <ThemeProvider theme={theme}>
+        <NavLink className={css.link} to="/register"><Button style={{ marginRight: 10 }} variant="contained">Register</Button>
+        
+        </NavLink>
+      < NavLink className={css.link} to="/login"><Button  variant="contained">Log In</Button>
+        
+      </NavLink>
+      </ThemeProvider>
+      
 
     </div>
   );
